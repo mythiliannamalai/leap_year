bd3d8b0806896d91d5ae62ab7a2b2f0182574936
mythili annamalai <mythiliannamalai45@gmail.com>
2022-03-05 20:24:25 +0530

    leap_or_not

---
+++ leap_year/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("enter the year");
+int year = int.Parse(Console.ReadLine());
+if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+{
+    Console.WriteLine("{0} is a leap year");
+}
+else
+{
+    Console.WriteLine("{0} is not leap year");
+}
+
+//ANOTHER METHOD
+
+//Console.WriteLine("enter the year");
+//int year = int.Parse(Console.ReadLine());
+//if (DateTime.IsLeapYear(year))
+//{
+//    Console.WriteLine("{0} is a leap year");
+//}
+//else
+//{
+//    Console.WriteLine("{0} is not leap year");
+//}
